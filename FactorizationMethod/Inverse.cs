20bb2b121f03e6086318b8ae7958c44ab76ef9cb
Павел Смирнов <cocacola76rus@gmail.com>
2023-06-01 18:31:21 +0300

    Добавьте файлы проекта.

---
+++ FactorizationMethod/Inverse.cs
@@ -0,0 +1,57 @@
+﻿using System.Numerics;
+
+namespace FactorizationMethod
+{
+    public class Inverse
+    {
+        private static BigInteger Egcd(BigInteger left,
+                              BigInteger right,
+                          out BigInteger leftFactor,
+                          out BigInteger rightFactor)
+        {
+            leftFactor = 0;
+            rightFactor = 1;
+            BigInteger u = 1;
+            BigInteger v = 0;
+            BigInteger gcd = 0;
+
+            while (left != 0)
+            {
+                BigInteger q = right / left;
+                BigInteger r = right % left;
+
+                BigInteger m = leftFactor - u * q;
+                BigInteger n = rightFactor - v * q;
+
+                right = left;
+                left = r;
+                leftFactor = u;
+                rightFactor = v;
+                u = m;
+                v = n;
+
+                gcd = right;
+            }
+
+            return gcd;
+        }
+
+        public static BigInteger gcd(BigInteger m, BigInteger n)
+        {
+            BigInteger x, y;
+            return Egcd(m,n,out x,out y);
+        }
+
+        public static BigInteger ModInverse(BigInteger value, BigInteger modulo)
+        {
+            BigInteger x, y;
+
+            Egcd(value, modulo, out x, out y);
+
+            if (x < 0)
+                x += modulo;
+
+            return x % modulo;
+        }
+    }
+}
---
+++ FactorizationMethod/Program.cs
@@ -0,0 +1,66 @@
+﻿using FactorizationMethod;
+using System;
+using System.Globalization;
+using System.Numerics;
+
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //вход
+            var n = 25;//8191 * 131071;//661643;// 3571*3559; //455839 //
+            var m = 12;
+            //генерация
+            while (true)
+            {
+                var random = new Random();
+
+                var a = random.Next(n);
+                var x = random.Next(n);
+                var y = random.Next(n);
+                var b = y * y - x * x * x + a * x;
+                var Q = new ECPoint(a, b, x, y, n);
+                var i = 0;
+                var pi = 2;
+                int ai = 0;
+
+                
+
+                var Qi = Q;
+                //ищем простое
+
+                while (i < m)
+                {
+                    while (!IsPrime(pi))
+                    {
+                        pi++;
+                    }
+
+                    i = i + 1;
+                    ai = (int)Math.Truncate(0.5 * Math.Log(n) / Math.Log(pi));
+                    var j = 0;
+
+                    while (j < ai)
+                    {
+                        Qi = ECPoint.multiply(pi, Qi);
+                        j = j + 1;
+                    }
+
+                    pi++;
+                }
+            }
+        }
+        public static bool IsPrime(int number)
+        {
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+    
+}
